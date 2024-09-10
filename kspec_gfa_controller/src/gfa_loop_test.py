2f1cbcf5e8591ce5416640d93026c91d6530fbf4
mmingyeong <girlalsrud@gmail.com>
2024-09-10 15:34:55 +0900

    loop test update

--- kspec_gfa_controller/src/gfa_loop_test.py
+++ kspec_gfa_controller/src/gfa_loop_test.py
@@ -1,14 +1,27 @@
 import asyncio
+from contextlib import suppress
 
-is_loop_running = True
 
-async def loop_function(is_loop_running):
-    while is_loop_running:
-        print("Loop is running...")
-        await asyncio.sleep(1)  # Asynchronous wait to prevent blocking
-        if not is_loop_running:
-            break
+class gfa_loop_test:
+    async def start_loop(self):
+        self._check_task = asyncio.create_task(self._check_loop())
         
-async def stop_loop():
-    is_loop_running = False
-    
+        return self
+            
+    async def stop_loop(self):
+        await self.cancel_task(self._check_task)
+
+    async def _check_loop(self):
+        while True:
+            print("Loop is running...")
+            await asyncio.sleep(1)  # Asynchronous wait to prevent blocking
+
+    async def cancel_task(self, task: asyncio.Future):
+        """Safely cancels a task."""
+
+        if task is None or task.done():
+            return
+
+        task.cancel()
+        with suppress(asyncio.CancelledError):
+            await task
